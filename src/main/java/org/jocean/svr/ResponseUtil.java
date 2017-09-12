106172e737bdb8697a0c59566c7133ce0bcc4e50
ming.ma <isdom.maming@gmail.com>
2017-09-12 19:17:52 +0800

    新增工具类 ResponseUtil: 常用 HttpResponse 响应

---
+++ src/main/java/org/jocean/svr/ResponseUtil.java
@@ -0,0 +1,40 @@
+package org.jocean.svr;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.netty.buffer.ByteBuf;
+import rx.Observable;
+
+public class ResponseUtil {
+    
+    @SuppressWarnings("unused")
+    private static final Logger LOG
+        = LoggerFactory.getLogger(ResponseUtil.class);
+    
+    private ResponseUtil() {
+        throw new IllegalStateException("No instances!");
+    }
+    
+    public static Observable<Object> statusOnly(final int status) {
+        return Observable.just(new StatusOnly(status));
+    }
+    
+    private static final class StatusOnly implements MessageResponse, MessageBody {
+        StatusOnly(final int status) {
+            this._status = status;
+        }
+        
+        @Override
+        public int status() {
+            return _status;
+        }
+
+        @Override
+        public ByteBuf content() {
+            return null;
+        }
+        
+        private final int _status;
+    }
+}
