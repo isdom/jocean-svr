62df585264d70cef377d2650619deca8ecafffa1
ming.ma <isdom.maming@gmail.com>
2018-02-15 23:32:21 +0800

    将 toZip 算子 从 ResponseUtil 独立到 ZipUtil 中，简化实现中私有方法的命名

--- src/main/java/org/jocean/svr/ResponseUtil.java
+++ src/main/java/org/jocean/svr/ResponseUtil.java
@@ -1,11 +1,7 @@
 package org.jocean.svr;
 
-import java.io.IOException;
 import java.io.OutputStream;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.zip.Deflater;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.core.MediaType;
@@ -13,11 +9,7 @@ import javax.ws.rs.core.MediaType;
 import org.jocean.http.DoFlush;
 import org.jocean.http.MessageUtil;
 import org.jocean.http.util.RxNettys;
-import org.jocean.idiom.DisposableWrapper;
-import org.jocean.idiom.DisposableWrapperUtil;
 import org.jocean.idiom.ExceptionUtils;
-import org.jocean.idiom.Terminable;
-import org.jocean.netty.util.ByteBufsOutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,12 +19,8 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.google.common.base.Charsets;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufInputStream;
 import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.DefaultHttpResponse;
-import io.netty.handler.codec.http.HttpContent;
-import io.netty.handler.codec.http.HttpHeaderNames;
-import io.netty.handler.codec.http.HttpHeaderValues;
 import io.netty.handler.codec.http.HttpObject;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponse;
@@ -40,12 +28,9 @@ import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpUtil;
 import io.netty.handler.codec.http.HttpVersion;
 import io.netty.handler.codec.http.LastHttpContent;
-import io.netty.util.CharsetUtil;
 import rx.Observable;
 import rx.Observable.Transformer;
-import rx.functions.Action0;
 import rx.functions.Action2;
-import rx.functions.Func0;
 import rx.functions.Func1;
 
 public class ResponseUtil {
@@ -317,115 +302,5 @@ public class ResponseUtil {
                 return responseRef.get();
             }
         };
-    }
-    
-    public static Observable.Transformer<HttpObject, Object> toZip(
-            final String zippedName,
-            final String contentName,
-            final Terminable terminable,
-            final Func0<ByteBuf> newBuffer,
-            final int bufsize) {
-        return new Observable.Transformer<HttpObject, Object>() {
-            @Override
-            public Observable<Object> call(final Observable<HttpObject> obsResponse) {
-                
-                final ByteBufsOutputStream bufout = new ByteBufsOutputStream(newBuffer, null);
-                final ZipOutputStream zipout = new ZipOutputStream(bufout, CharsetUtil.UTF_8);
-                zipout.setLevel(Deflater.BEST_COMPRESSION);
-                
-                final byte[] readbuf = new byte[bufsize];
-                
-                terminable.doOnTerminate(() -> {
-                    try {
-                        zipout.close();
-                    } catch (IOException e1) {
-                    }
-                });
-                
-                return obsResponse.flatMap(RxNettys.splitFullHttpMessage())
-                .flatMap(httpobj -> {
-                    if (httpobj instanceof HttpResponse) {
-                        return Observable.concat(onResponse((HttpResponse)httpobj, zippedName), 
-                                bufout2bufs(bufout, addZipEntry(zipout, contentName)).map(todwb(terminable)));
-                    } else if (httpobj instanceof HttpContent) {
-                        final HttpContent content = (HttpContent)httpobj;
-                        if (content.content().readableBytes() == 0) {
-                            return Observable.empty();
-                        } else {
-                            return bufout2bufs(bufout, zipContent(zipout, content, readbuf)).map(todwb(terminable));
-                        }
-                    } else {
-                        return Observable.just(httpobj);
-                    }},
-                    e -> Observable.error(e),
-                    () -> Observable.concat(bufout2bufs(bufout, finishZip(zipout)).map(todwb(terminable)), 
-                            Observable.just(LastHttpContent.EMPTY_LAST_CONTENT))
-                );
-            }
-        };
-    }
-
-    private static Observable<? extends Object> onResponse(final HttpResponse resp,  final String zipedName) {
-        HttpUtil.setTransferEncodingChunked(resp, true);
-        resp.headers().set(HttpHeaderNames.CONTENT_TYPE, HttpHeaderValues.APPLICATION_OCTET_STREAM);
-        resp.headers().set(HttpHeaderNames.CONTENT_DISPOSITION, "attachment; filename=" + zipedName);
-        return Observable.just(resp);
-    }
-    
-    private static Action0 addZipEntry(final ZipOutputStream zipout, final String contentName) {
-        return ()-> {
-            try {
-                final ZipEntry entry = new ZipEntry(contentName);
-                zipout.putNextEntry(entry);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        };
-    }
-    
-    private static Action0 zipContent(final ZipOutputStream zipout, final HttpContent content, final byte[] readbuf) {
-        return ()->{
-            try (final ByteBufInputStream is = new ByteBufInputStream(content.content())) {
-                int readed;
-                while ((readed = is.read(readbuf)) > 0) {
-                    zipout.write(readbuf, 0, readed);
-                }
-                zipout.flush();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        };
-    }
-
-    private static Action0 finishZip(final ZipOutputStream zipout) {
-        return ()->{
-            try {
-                zipout.closeEntry();
-                zipout.finish();
-                zipout.close();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        };
-    }
-
-    private static Observable<ByteBuf> bufout2bufs(final ByteBufsOutputStream bufout, final Action0 fillcontent) {
-        return Observable.unsafeCreate(subscriber -> {
-            if (!subscriber.isUnsubscribed()) {
-                bufout.setOnBuffer(buf->subscriber.onNext(buf));
-                try {
-                    fillcontent.call();
-                    subscriber.onCompleted();
-                } catch (Exception e) {
-                    subscriber.onError(e);
-                } finally {
-                    bufout.setOnBuffer(null);
-                }
-            }
-        });
-    }
-
-    private static Func1<ByteBuf, DisposableWrapper<ByteBuf>> todwb(final Terminable terminable) {
-        return DisposableWrapperUtil.<ByteBuf>wrap(RxNettys.<ByteBuf>disposerOf(), terminable);
     }
 }
