b5164c89c173c1f50353b8470a175a2f6f5e7900
ming.ma <isdom.maming@gmail.com>
2018-03-16 11:49:54 +0800

    增加使用 MessageUtil.interact 得到 Interact 实例的 interactWith 版本

--- src/main/java/org/jocean/svr/ControllerUtil.java
+++ src/main/java/org/jocean/svr/ControllerUtil.java
@@ -2,6 +2,7 @@ package org.jocean.svr;
 
 import org.jocean.http.Interact;
 import org.jocean.http.InteractBuilder;
+import org.jocean.http.MessageUtil;
 import org.jocean.http.client.HttpClient;
 import org.jocean.idiom.BeanFinder;
 import org.jocean.idiom.Pair;
@@ -23,6 +24,17 @@ public class ControllerUtil {
     public static <API, T> Observable<T> interactWith(final BeanFinder finder, final Class<API> apicls,
             final InteractBuilder ib, final Func2<API, Interact, Observable<T>> callapi) {
         return Observable.zip(finder.find(apicls), finder.find(HttpClient.class).map(client -> ib.interact(client)),
+                (api, interact) -> Pair.of(api, interact)).flatMap(pair -> {
+                    final API api = pair.first;
+                    final Interact interact = pair.second;
+                    return callapi.call(api, interact);
+                });
+    }
+
+    public static <API, T> Observable<T> interactWith(final BeanFinder finder, final Class<API> apicls,
+            final Func2<API, Interact, Observable<T>> callapi) {
+        return Observable.zip(finder.find(apicls), 
+                finder.find(HttpClient.class).map(client -> MessageUtil.interact(client)),
                 (api, interact) -> Pair.of(api, interact)).flatMap(pair -> {
                     final API api = pair.first;
                     final Interact interact = pair.second;
