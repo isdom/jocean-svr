b9e450763399ec0b3c4088f65d958121c9788df6
ming.ma <isdom.maming@gmail.com>
2018-03-19 17:08:07 +0800

    ControllerUtil 工具类: 移除 interactWith 方法, 增加 interacts 方法

--- src/main/java/org/jocean/svr/ControllerUtil.java
+++ src/main/java/org/jocean/svr/ControllerUtil.java
@@ -5,12 +5,10 @@ import org.jocean.http.InteractBuilder;
 import org.jocean.http.MessageUtil;
 import org.jocean.http.client.HttpClient;
 import org.jocean.idiom.BeanFinder;
-import org.jocean.idiom.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import rx.Observable;
-import rx.functions.Func2;
 
 public class ControllerUtil {
     @SuppressWarnings("unused")
@@ -21,24 +19,11 @@ public class ControllerUtil {
         throw new IllegalStateException("No instances!");
     }
 
-    public static <API, T> Observable<T> interactWith(final BeanFinder finder, final Class<API> apicls,
-            final InteractBuilder ib, final Func2<API, Interact, Observable<T>> callapi) {
-        return Observable.zip(finder.find(apicls), finder.find(HttpClient.class).map(client -> ib.interact(client)),
-                (api, interact) -> Pair.of(api, interact)).flatMap(pair -> {
-                    final API api = pair.first;
-                    final Interact interact = pair.second;
-                    return callapi.call(api, interact);
-                });
+    public static Observable<Interact> interacts(final BeanFinder finder, final InteractBuilder ib) {
+        return finder.find(HttpClient.class).map(client-> ib.interact(client));
     }
 
-    public static <API, T> Observable<T> interactWith(final BeanFinder finder, final Class<API> apicls,
-            final Func2<API, Interact, Observable<T>> callapi) {
-        return Observable.zip(finder.find(apicls), 
-                finder.find(HttpClient.class).map(client -> MessageUtil.interact(client)),
-                (api, interact) -> Pair.of(api, interact)).flatMap(pair -> {
-                    final API api = pair.first;
-                    final Interact interact = pair.second;
-                    return callapi.call(api, interact);
-                });
+    public static Observable<Interact> interacts(final BeanFinder finder) {
+        return finder.find(HttpClient.class).map(client-> MessageUtil.interact(client));
     }
 }
