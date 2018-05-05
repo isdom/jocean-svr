14736c5c0b1b12e09f6d40a7fc538f4b750daa5d
ming.ma <isdom.maming@gmail.com>
2018-05-05 13:38:54 +0800

    增加可测试性

--- src/main/java/org/jocean/svr/ResponseUtil.java
+++ src/main/java/org/jocean/svr/ResponseUtil.java
@@ -33,38 +33,38 @@ import rx.Observable.Transformer;
 import rx.functions.Func1;
 
 public class ResponseUtil {
-    
+
     private static final Logger LOG
         = LoggerFactory.getLogger(ResponseUtil.class);
-    
+
     private ResponseUtil() {
         throw new IllegalStateException("No instances!");
     }
-    
+
     static private class ACRHeader {
         @HeaderParam("access-control-request-headers")
         private String _headers;
-        
+
         @HeaderParam("access-control-request-method")
         private String _method;
-    
+
         @HeaderParam("origin")
         private String _origin;
     }
-    
+
     static private class ACAOnly implements MessageResponse, ResponseBody {
         @HeaderParam("access-control-allow-headers")
         private String _headers;
-        
+
         @HeaderParam("access-control-allow-methods")
         private String _method;
-    
+
         @HeaderParam("access-control-allow-origin")
         private String _origin;
 
         @HeaderParam("access-control-allow-credentials")
-        private boolean _credentials = true;
-        
+        private final boolean _credentials = true;
+
         @Override
         public ByteBuf content() {
             return null;
@@ -75,7 +75,7 @@ public class ResponseUtil {
             return 202;
         }
     }
-    
+
     public static Observable<Object> acceptCORS(final Observable<HttpObject> request) {
         final ACRHeader hdr = new ACRHeader();
         return request.compose(RxNettys.asHttpRequest())
@@ -91,58 +91,62 @@ public class ResponseUtil {
                     }})
                 .delaySubscription(request.last());
     }
-    
+
     public static Object flushOnly() {
         return DoFlush.Util.flushOnly();
     }
-    
+
     public static Object statusOnly(final int status) {
         return new StatusOnly(status);
     }
-    
-    private static class Redirectable implements MessageResponse, ResponseBody {
+
+    public static class Redirectable implements MessageResponse, ResponseBody {
 
         public Redirectable(final String location) {
             this._location = location;
         }
-        
+
         @Override
         public int status() {
             return 302;
         }
-        
+
         @Override
         public ByteBuf content() {
             return null;
         }
 
+        public String location() {
+            return this._location;
+        }
+
         @HeaderParam("location")
-        private String _location;
+        private final String _location;
     }
-    
+
     public static Object redirectOnly(final String location) {
         return new Redirectable(location);
     }
-    
+
     public static Object responseAsJson(final int status, final Object pojo) {
         return new FullResponse(status, MediaType.APPLICATION_JSON, Unpooled.wrappedBuffer(JSON.toJSONBytes(pojo)));
     }
-    
+
     public static Object responseAsXml(final int status, final Object pojo) {
         try {
             final XmlMapper mapper = new XmlMapper();
             return new FullResponse(status, MediaType.APPLICATION_XML,
                     Unpooled.wrappedBuffer(mapper.writeValueAsBytes(pojo)));
-        } catch (JsonProcessingException e) {
+        } catch (final JsonProcessingException e) {
             LOG.warn("exception when convert {} to xml, detail: {}", pojo, ExceptionUtils.exception2detail(e));
             return statusOnly(500);
         }
     }
-    
+
     public static Object responseAsText(final int status, final String text) {
         return new FullResponse(status, MediaType.TEXT_PLAIN, Unpooled.wrappedBuffer(text.getBytes(Charsets.UTF_8)));
     }
-    
+
     public static MessageResponse respWithStatus(final int status) {
         return new MessageResponse() {
             @Override
@@ -150,7 +154,7 @@ public class ResponseUtil {
                 return status;
             }};
     }
-    
+
     public static Transformer<Object, Object> handleExpect100(
             final Observable<HttpObject> request,
             final Func1<HttpRequest, Integer> continueHandler) {
@@ -167,7 +171,7 @@ public class ResponseUtil {
                             final int status = continueHandler.call(req);
                             if (status == 100) {
                                 return Observable.concat(
-                                    Observable.<Object>just(new StatusOnly(100), DoFlush.Util.flushOnly()), 
+                                    Observable.<Object>just(new StatusOnly(100), DoFlush.Util.flushOnly()),
                                     response);
                             } else {
                                 return Observable.<Object>just(ResponseUtil.statusOnly(status));
@@ -176,7 +180,7 @@ public class ResponseUtil {
                     }});
             }};
     }
-    
+
     private static final Transformer<Object, Object> DEFAULT_ERROR_HANDLER = new Transformer<Object, Object>() {
         @Override
         public Observable<Object> call(final Observable<Object> response) {
@@ -184,28 +188,28 @@ public class ResponseUtil {
                     null != error.getMessage() ? error.getMessage() : ExceptionUtils.exception2detail(error))));
         }
     };
-    
+
     public static Transformer<Object, Object> defaultErrorHandler() {
         return DEFAULT_ERROR_HANDLER;
     }
-    
+
     public static ResponseBody emptyBody() {
         return EMPTY_BODY;
     }
-    
+
     private static final ResponseBody EMPTY_BODY = new ResponseBody() {
         @Override
         public ByteBuf content() {
             return null;
         }};
-    
+
     private static final class FullResponse implements MessageResponse, ResponseBody {
         FullResponse(final int status, final String contentType, final ByteBuf content) {
             this._status = status;
             this._contentType = contentType;
             this._content = content;
         }
-        
+
         @Override
         public int status() {
             return this._status;
@@ -215,19 +219,19 @@ public class ResponseUtil {
         public ByteBuf content() {
             return this._content;
         }
-        
+
         private final int _status;
         private final ByteBuf _content;
-        
+
         @HeaderParam("content-type")
         private final String _contentType;
     }
-        
+
     private static final class StatusOnly implements MessageResponse, ResponseBody {
         StatusOnly(final int status) {
             this._status = status;
         }
-        
+
         @Override
         public int status() {
             return _status;
@@ -237,24 +241,24 @@ public class ResponseUtil {
         public ByteBuf content() {
             return null;
         }
-        
+
         private final int _status;
     }
-    
+
     public static Observable<Object> response(final int status) {
-        return Observable.<Object>just(new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.valueOf(status)), 
+        return Observable.<Object>just(new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.valueOf(status)),
                 LastHttpContent.EMPTY_LAST_CONTENT);
     }
-    
+
     public interface ResponseBuilder {
-        
+
         public ResponseBuilder status(final int status);
-        
+
         public ResponseBuilder body(final Observable<? extends MessageBody> body);
-        
+
         public Observable<Object> build();
     }
-    
+
     public static ResponseBuilder response() {
         final AtomicReference<Observable<Object>> responseRef = new AtomicReference<>(response(200));
         return new ResponseBuilder() {
