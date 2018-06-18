47413f07db7ffcb2d936c4a0957931f05d5aaa8f
ming.ma <isdom.maming@gmail.com>
2018-06-18 22:05:13 +0800

    DefaultRpcConfig 为 RpcConfig 的缺省实现,  新增 root_rpccfg.xml

---
+++ src/main/java/org/jocean/svr/DefaultRpcConfig.java
@@ -0,0 +1,56 @@
+package org.jocean.svr;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.jocean.http.Interact;
+import org.jocean.idiom.BeanFinder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.StringUtils;
+
+import rx.Observable.Transformer;
+
+public class DefaultRpcConfig implements RpcConfig {
+
+    public DefaultRpcConfig() {
+        this._children = null;
+    }
+
+    public DefaultRpcConfig(final Map<String, RpcConfig> children) {
+        this._children = children;
+    }
+
+    @Override
+    public Transformer<Interact, Interact> before() {
+        return interacts -> interacts.compose(FinderUtil.processors(this._finder, this._before));
+    }
+
+    @Override
+    public <T> Transformer<T, T> after() {
+        return source -> source.compose(FinderUtil.processors(this._finder, this._after));
+    }
+
+    @Override
+    public RpcConfig child(final String name) {
+        return null != this._children ? this._children.get(name) : null;
+    }
+
+    @Value("${before}")
+    public void setBefore(final String before) {
+        this._before = StringUtils.commaDelimitedListToStringArray(before);
+    }
+
+    @Value("${after}")
+    public void setAfter(final String after) {
+        this._after = StringUtils.commaDelimitedListToStringArray(after);
+    }
+
+    private String[] _before = new String[0];
+    private String[] _after = new String[0];
+
+    @Inject
+    private BeanFinder _finder;
+
+    private final Map<String, RpcConfig> _children;
+}
