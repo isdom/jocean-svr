5744b947f3926fc969812ae4d28e79e6d1a82f82
ming.ma <isdom@qq.com>
2018-09-13 17:18:56 +0800

    将 MessageBodyUsingHolder 作为 MultipartBody 的内嵌类

--- src/main/java/org/jocean/svr/MultipartBody.java
+++ src/main/java/org/jocean/svr/MultipartBody.java
@@ -3,15 +3,21 @@ package org.jocean.svr;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jocean.http.ByteBufSlice;
 import org.jocean.http.MessageBody;
 import org.jocean.http.MessageUtil;
 import org.jocean.http.server.HttpServerBuilder.HttpTrade;
 import org.jocean.http.util.Nettys;
+import org.jocean.http.util.RxNettys;
 import org.jocean.idiom.DisposableWrapper;
+import org.jocean.idiom.DisposableWrapperUtil;
 import org.jocean.idiom.ExceptionUtils;
+import org.jocean.idiom.Terminable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufHolder;
 import io.netty.handler.codec.http.HttpContent;
 import io.netty.handler.codec.http.HttpObject;
 import io.netty.handler.codec.http.HttpRequest;
@@ -23,6 +29,7 @@ import io.netty.handler.codec.http.multipart.HttpPostRequestDecoder.EndOfDataDec
 import io.netty.handler.codec.http.multipart.HttpPostRequestDecoder.ErrorDataDecoderException;
 import io.netty.handler.codec.http.multipart.InterfaceHttpData;
 import rx.Observable;
+import rx.Observable.OnSubscribe;
 import rx.Subscriber;
 import rx.functions.Func1;
 import rx.subscriptions.Subscriptions;
@@ -146,5 +153,82 @@ class MultipartBody implements Observable.OnSubscribe<MessageBody> {
                 fileUpload.getContentType(),
                 fileUpload.getFilename(),
                 fileUpload.getName());
+    }
+
+    private static class MessageBodyUsingHolder implements MessageBody {
+
+        @SuppressWarnings("unused")
+        private static final Logger LOG
+            = LoggerFactory.getLogger(MessageBodyUsingHolder.class);
+
+        public MessageBodyUsingHolder(
+                final Terminable terminable,
+                final ByteBufHolder holder,
+                final int contentLength,
+                final String contentType,
+                final String filename,
+                final String name
+                ) {
+            this._terminable = terminable;
+            this._holder = holder;
+            this._contentLength = contentLength;
+            this._contentType = contentType;
+            this._filename = filename;
+            this._name = name;
+
+            terminable.doOnTerminate(() -> {
+                if (null != holder) {
+                    holder.release();
+                }
+            });
+        }
+
+        @Override
+        public String contentType() {
+            return this._contentType;
+        }
+
+        @Override
+        public int contentLength() {
+            return this._contentLength;
+        }
+
+        @Override
+        public Observable<? extends ByteBufSlice> content() {
+            return Observable.just(new ByteBufSlice() {
+
+                @Override
+                public void step() {}
+
+                @Override
+                public Observable<? extends DisposableWrapper<? extends ByteBuf>> element() {
+                    return Observable.unsafeCreate(new OnSubscribe<DisposableWrapper<ByteBuf>>() {
+                        @Override
+                        public void call(final Subscriber<? super DisposableWrapper<ByteBuf>> subscriber) {
+                            if (!subscriber.isUnsubscribed()) {
+                                try {
+                                    final ByteBuf buf = _holder.content().retainedSlice();
+                                    if (null != buf) {
+                                        subscriber.onNext(DisposableWrapperUtil.disposeOn(_terminable, RxNettys.wrap4release(buf)));
+                                        subscriber.onCompleted();
+                                    } else {
+                                        subscriber.onError(new RuntimeException("invalid bytebuf"));
+                                    }
+                                } catch (final Exception e) {
+                                    subscriber.onError(e);
+                                }
+                            }
+                        }
+                    });
+                }});
+
+        }
+
+        private final Terminable _terminable;
+        private final ByteBufHolder _holder;
+        private final int    _contentLength;
+        private final String _contentType;
+        private final String _filename;
+        private final String _name;
     }
 }
