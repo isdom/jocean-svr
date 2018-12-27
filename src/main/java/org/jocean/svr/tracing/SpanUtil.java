a00c591b6a4a42a178aeb49347d656496de0286e
ming.ma <isdom@qq.com>
2018-12-27 18:47:49 +0800

    try fix "Bad type on operand stack" by NOT using lambda expr

--- src/main/java/org/jocean/svr/tracing/SpanUtil.java
+++ src/main/java/org/jocean/svr/tracing/SpanUtil.java
@@ -25,61 +25,65 @@ public class SpanUtil {
         final StringBuilder bodysb4span = new StringBuilder();
         final AtomicInteger bodysize4span = new AtomicInteger(0);
 
-        return fullmsgs -> fullmsgs.map(fullmsg -> {
-            final BufsInputStream<ByteBuf> bufsin = new BufsInputStream<>(buf -> buf, buf -> {});
+        return new Transformer<FullMessage<MSG>, FullMessage<MSG>>() {
+            @Override
+            public Observable<FullMessage<MSG>> call(final Observable<FullMessage<MSG>> fullmsgs) {
+                return fullmsgs.map(fullmsg -> {
+                    final BufsInputStream<ByteBuf> bufsin = new BufsInputStream<>(buf -> buf, buf -> {});
 
-            bufsin.markEOS();
+                    bufsin.markEOS();
 
-            return (FullMessage<MSG>)new FullMessage<MSG>() {
-                @Override
-                public MSG message() {
-                    return fullmsg.message();
-                }
-                @Override
-                public Observable<? extends MessageBody> body() {
-                    return fullmsg.body().map(body -> {
-                        span.setTag("http.contenttype", body.contentType());
-                        span.setTag("http.contentlength", body.contentLength());
-                        return (MessageBody)new MessageBody() {
-                            @Override
-                            public String contentType() {
-                                return body.contentType();
-                            }
-                            @Override
-                            public int contentLength() {
-                                return body.contentLength();
-                            }
-                            @Override
-                            public Observable<? extends ByteBufSlice> content() {
-                                return body.content().doOnNext(bbs -> {
-                                    if (bodysize4span.get() < maxLogSize) {
-                                        final Iterator<? extends DisposableWrapper<? extends ByteBuf>> iter =
-                                                bbs.element().iterator();
-                                        for (;iter.hasNext();) {
-                                            final ByteBuf buf = iter.next().unwrap();
-                                            if (buf.readableBytes() > 0 && bodysize4span.get() < maxLogSize) {
-                                                final int length = Math.min(maxLogSize - bodysize4span.get(),
-                                                        buf.readableBytes());
-                                                bufsin.appendBuf(buf.slice(0, length));
-                                                bodysize4span.addAndGet(length);
-                                            }
-                                            if (bodysize4span.get() >= maxLogSize) {
-                                                break;
-                                            }
-                                        }
-                                        try {
-                                            bodysb4span.append(new String(ByteStreams.toByteArray(bufsin), Charsets.UTF_8));
-                                        } catch (final Exception e) {}
+                    return (FullMessage<MSG>)new FullMessage<MSG>() {
+                        @Override
+                        public MSG message() {
+                            return fullmsg.message();
+                        }
+                        @Override
+                        public Observable<? extends MessageBody> body() {
+                            return fullmsg.body().map(body -> {
+                                span.setTag("http.contenttype", body.contentType());
+                                span.setTag("http.contentlength", body.contentLength());
+                                return (MessageBody)new MessageBody() {
+                                    @Override
+                                    public String contentType() {
+                                        return body.contentType();
                                     }
-                                });
-                            }};
-                    });
-                }};
-        })
-        .doOnTerminate(() -> {
-            if (bodysize4span.get() > 0) {
-                span.log(Collections.singletonMap(logname, bodysb4span.toString()));
-            }
-        });
+                                    @Override
+                                    public int contentLength() {
+                                        return body.contentLength();
+                                    }
+                                    @Override
+                                    public Observable<? extends ByteBufSlice> content() {
+                                        return body.content().doOnNext(bbs -> {
+                                            if (bodysize4span.get() < maxLogSize) {
+                                                final Iterator<? extends DisposableWrapper<? extends ByteBuf>> iter =
+                                                        bbs.element().iterator();
+                                                for (;iter.hasNext();) {
+                                                    final ByteBuf buf = iter.next().unwrap();
+                                                    if (buf.readableBytes() > 0 && bodysize4span.get() < maxLogSize) {
+                                                        final int length = Math.min(maxLogSize - bodysize4span.get(),
+                                                                buf.readableBytes());
+                                                        bufsin.appendBuf(buf.slice(0, length));
+                                                        bodysize4span.addAndGet(length);
+                                                    }
+                                                    if (bodysize4span.get() >= maxLogSize) {
+                                                        break;
+                                                    }
+                                                }
+                                                try {
+                                                    bodysb4span.append(new String(ByteStreams.toByteArray(bufsin), Charsets.UTF_8));
+                                                } catch (final Exception e) {}
+                                            }
+                                        });
+                                    }};
+                            });
+                        }};
+                })
+                .doOnTerminate(() -> {
+                    if (bodysize4span.get() > 0) {
+                        span.log(Collections.singletonMap(logname, bodysb4span.toString()));
+                    }
+                });
+            }};
     }
 }
