899c1392fd0baf4e268e1bc3bd3564666ab5d5e9
ming.ma <isdom@qq.com>
2019-07-26 21:44:28 +0800

    impl prometheus exporter using jocean-http

---
+++ src/main/java/org/jocean/svr/prometheus/ExporterController.java
@@ -0,0 +1,74 @@
+package org.jocean.svr.prometheus;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.util.Enumeration;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import org.jocean.http.WriteCtrl;
+import org.jocean.idiom.DisposableWrapperUtil;
+import org.jocean.idiom.Stepable;
+import org.jocean.svr.WithStepable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import io.prometheus.client.Collector;
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.exporter.common.TextFormat;
+import rx.Observable;
+import rx.functions.Action2;
+
+@Controller
+@Scope("singleton")
+public class ExporterController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ExporterController.class);
+
+    @Path("/metrics")
+    @GET
+    public WithStepable<Stepable<Enumeration<Collector.MetricFamilySamples>>> getMetrics(final WriteCtrl writeCtrl) {
+
+        writeCtrl.sended().subscribe(obj -> DisposableWrapperUtil.dispose(obj));
+
+        return new WithStepable<Stepable<Enumeration<Collector.MetricFamilySamples>>>() {
+            @Override
+            public String contentType() {
+                return TextFormat.CONTENT_TYPE_004;
+            }
+
+            @Override
+            public Observable<Stepable<Enumeration<Collector.MetricFamilySamples>>> stepables() {
+                return Observable.just(new Stepable<Enumeration<Collector.MetricFamilySamples>>() {
+                            @Override
+                            public void step() {}
+
+                            @Override
+                            public Enumeration<Collector.MetricFamilySamples> element() {
+                                return  _registry.filteredMetricFamilySamples(/*parseQuery(query)*/Collections.emptySet());
+                            }
+                        });
+            }
+
+            @Override
+            public Action2<Stepable<Enumeration<Collector.MetricFamilySamples>>, OutputStream> output() {
+                return (stepable, out) -> {
+                    try(final OutputStreamWriter osw = new OutputStreamWriter(out)) {
+                        TextFormat.write004(osw, stepable.element());
+                        osw.flush();
+                    } catch (final IOException e) {
+//                        // TODO Auto-generated catch block
+//                        e.printStackTrace();
+                    }
+                };
+            }
+        };
+    }
+
+    CollectorRegistry _registry = CollectorRegistry.defaultRegistry;
+}
