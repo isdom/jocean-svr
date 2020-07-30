ff79200ec46d69798d281e132509a1fc2eb479d5
ming.ma <isdom@qq.com>
2020-07-30 15:53:39 +0800

    限定 RpcFacade 注解只能用于 参数及成员字段

--- src/main/java/org/jocean/svr/annotation/RpcFacade.java
+++ src/main/java/org/jocean/svr/annotation/RpcFacade.java
@@ -1,9 +1,12 @@
 package org.jocean.svr.annotation;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.PARAMETER, ElementType.FIELD})
 public @interface RpcFacade {
     // pre processors
     String[] value();
