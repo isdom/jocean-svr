3c6fb49a4c8203ba8515848a2a11129ede97ece8
ming.ma <isdom@qq.com>
2021-01-25 15:41:08 +0800

    实现 @JService 字段初始化的异步

--- src/main/java/org/jocean/svr/JServiceBuilder.java
+++ src/main/java/org/jocean/svr/JServiceBuilder.java
@@ -1,5 +1,9 @@
 package org.jocean.svr;
 
+/**
+ * @deprecated use {@link #JService} instead.
+ */
+@Deprecated
 public interface JServiceBuilder {
     <S> S build(final String serviceName, final Class<S> serviceType, final Object... args);
     <S> S build(final Class<S> serviceType, final Object... args);
