e5a387ecb179cb7f32fa3fa19d0529237c2a5a6a
ming.ma <isdom@qq.com>
2021-04-24 16:53:28 +0800

    add more detail log

--- src/main/java/org/jocean/svr/Registrar.java
+++ src/main/java/org/jocean/svr/Registrar.java
@@ -235,7 +235,7 @@ public class Registrar implements BeanHolderAware, MBeanRegisterAware {
                         if (null != bean) {
                             final HashMap<String, Object> target = new HashMap<String, Object>();
                             BeanUtils.copyProperties(bean, target);
-                            LOG.info("http.req.bean: {}", target);
+                            LOG.info("http.req.bean: {}/target: {}", bean, target);
                             _span.log(Collections.singletonMap("http.req.bean", target));
                         } else {
                             LOG.info("http.req.bean: (null)");
